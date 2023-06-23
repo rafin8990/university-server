8d94669f1ba5572ae2321f3d397fd339a0fb609b
rafin8990 <rafin8990@gmail.com>
2023-06-24 01:52:35 +0600

    setup server is completed

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
-import express, { Application } from 'express'
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 const app: Application = express()
-const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('University Server successfully connected')
 })
 
 export default app
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function main() {
   try {
-    await mongoose.connect('mongodb://127.0.0.1.27017/test')
+    await mongoose.connect(config.database_url as string)
     console.log(`Database connected successfully`)
+    app.listen(config.port, () => {
+      console.log(` application listening on port ${config.port}`)
+    })
   } catch (error) {
     console.log(`Failed to connect database `, error)
   }
