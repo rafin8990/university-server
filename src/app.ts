079d852a4c36493e220ac308dc0659b97b35622d
rafin8990 <rafin8990@gmail.com>
2023-06-27 22:36:10 +0600

    logger servicce controller added

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
-import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import express, { Application, Request, Response } from 'express'
+import { UserRoutes } from './app/Modules/user/user.routes'
+import globalErrorHandler from './app/middlewares/globalErrorHandler'
 const app: Application = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use('/api/v1/users', UserRoutes)
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', async (req: Request, res: Response) => {
   res.send('University Server successfully connected')
 })
+app.use(globalErrorHandler)
 
 export default app
