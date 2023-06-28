6793b872eb3eb1c962334bf3180c07727ec8a64f
rafin8990 <rafin8990@gmail.com>
2023-06-28 14:46:04 +0600

    academic semister added

--- src/Shared/logger.ts
+++ src/Shared/logger.ts
@@ -45,7 +45,7 @@ const errorlogger = createLogger({
         process.cwd(),
         'logs',
         'winston',
-        'successes',
+        'Errors',
         'UM-%DATE%-error.log'
       ),
       datePattern: 'YYYY-DD-MM-HH',
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import cors from 'cors'
 import express, { Application, Request, Response } from 'express'
+import { AcademicSemesterRoutes } from './app/Modules/academicSemister/academicSemister.route'
 import { UserRoutes } from './app/Modules/user/user.routes'
 import globalErrorHandler from './app/middlewares/globalErrorHandler'
 const app: Application = express()
@@ -8,6 +9,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/api/v1/users', UserRoutes)
+app.use('/api/v1/academic-semister', AcademicSemesterRoutes)
 
 app.get('/', async (req: Request, res: Response) => {
   res.send('University Server successfully connected')
